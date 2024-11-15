e11309a438c42019cfb68897b568db11525d3ba7
SergioDevSousa <oxentesergiosousa76@outlook.com>
2024-11-15 14:12:12 -0300

    #feat: Update responsividade telas menores

--- src/components/Information/index.jsx
+++ src/components/Information/index.jsx
@@ -5,13 +5,13 @@ import styles from './Information.module.css';
 
 function Information(){
     return(
-            <section className={styles.home}>
+            <div className={styles.home}>
                 <div className={styles.informacoes}>
                     <Digitacao text = "Olá! Sou Sérgio Sousa." />
                     <p>Estou cursando Análise e Desenvolvimento de Sistemas, atualmente finalizei a formação Front-End 2024 T6 ONE junto a ALURA e atualmente estudando na DIO. Este portifólio servirá para apresentar meus projetos desenvolvidos ao logo de meu aprendizado, assim como atualizar meus trabalhos. Espero contribuir e compartilhar com outros desenvolvedores e faço um convite para me seguir em minhas redes sociais.</p>
                 </div>
-                    <Perfil />
-            </section>
+                <Perfil />
+            </div>
     )
 }
 export default Information;
--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -3,7 +3,7 @@ import styles from './Perfil.module.css'
 function Perfil() {
     return (
         <div className={styles.fotoPerfil}>
-                <img src="./imagens/foto_perfil.png" alt="Perfil-dev" />
+            <img src="./imagens/foto_perfil.png" alt="Perfil-dev" />
         </div>
     );
 }
